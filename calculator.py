0ef43600248ea03f42b3262292c7acbe424ad268
drishtantR1508 <drishtant.rai.min19@itbhu.ac.in>
2020-10-14 22:40:00 +0530

    Adding new feature for checking if the number is prime.

--- calculator.py
+++ calculator.py
@@ -7,10 +7,11 @@ print("5.Square")
 print("6.Power")
 print("7.Square Root")
 print("8.Odd/Even")
-print("9.Quit")
+print("9.Check if Prime")
+print("10.Quit")
 while True:
     option = int(input("Enter a number to excute operation:  "))
-    if option == 9:
+    if option == 10:
         break
     elif option == 1:
         number1 = float(input("Enter first number: "))
@@ -44,6 +45,30 @@ while True:
             print("Even")
         else:
             print("Odd")
-    else : 
+    elif option == 9:
+        def prime_no(n) :
+            if (n <= 1) :
+                return False
+            if (n <= 3) :
+                return True
+            if (n % 2 == 0 or n % 3 == 0) :
+                return False
+            temp = 5
+            while(temp * temp <= n) :
+                if (n % temp == 0 or n % (temp + 2) == 0) :
+                    return False
+                temp = temp + 6
+
+            return True
+        number = int(input("Enter a number to check if it is a prime no: "))
+        print("You entered {}.".format(number))
+
+        if prime_no(number):
+            print("And the number {} is a Prime Number".format(number))
+        else:
+            print("And the number {} is not a Prime Number.".format(number))
+
+
+    else :
         print("Invalid Input")
 print("EXIT")
